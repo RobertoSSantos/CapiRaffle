3226af9de35728acebcc229d94b5c5f46d1eb519
Roberto Santos <rssan03@gmail.com>
2023-06-09 19:33:32 -0300

    Gol

--- app/src/main/java/br/com/senaicimatec/capiraffle/MainActivity.java
+++ app/src/main/java/br/com/senaicimatec/capiraffle/MainActivity.java
@@ -20,92 +20,10 @@ import br.com.senaicimatec.capiraffle.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding =ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        // Define o primeiro fragmento a aparecer na tela
-        replaceFragment(new HomeFragment());
-        binding.bottomNavView.setBackground(null);
-
-        binding.bottomNavView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
-                case R.id.homeMenu:
-                    replaceFragment(new HomeFragment());
-                    break;
-                case R.id.profileMenu:
-                    //replaceFragment(new LoginFragment());
-                    break;
-            }
-            return true;
-        });
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showBottomDialog();
-            }
-        });
-
-    }
-
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, fragment);
-        fragmentTransaction.commit();
-    }
-
-    private void showBottomDialog(){
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.bottom_sheet_dialog);
-
-        LinearLayout playlistLayout = dialog.findViewById(R.id.layoutPlaylist);
-        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
-
-        playlistLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-                replaceFragment(new AddRaffleFragment());
-            }
-        });
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }
-
-/* Tabela Cria Rifa:
-- id_rifa
-- nome do criador
-- cpf do criador
-- chave pix do criador
-- titulo da rifa
-- data termino
-- premio
-- valor de cada numero
- */
-
-/* Tabela Compra Rifa:
-- id_compra
-- nome comprador
-- cpf comprador
-- id da rifa - FK
-- numeros escolhidos
- */
